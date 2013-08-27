b0a058e56de651f908bd4c4aa8b0a3d468cdda36
Mikhail <zcmx@yandex.ru>
2013-08-27 15:13:12 +0400

    compass 0.1

--- src/com/example/Compass/CompassActivity.java
+++ src/com/example/Compass/CompassActivity.java
@@ -1,15 +1,78 @@
 package com.example.Compass;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import view.CompassView;
 
 public class CompassActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+    float[] aValues = new float[3];
+    float[] mValues = new float[3];
+    CompassView compassView;
+    SensorManager sensorManager;
+    private final SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+                aValues = event.values;
+            }
+            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
+                mValues = event.values;
+            }
+            updateOrientation(calculateOrientation());
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+    };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        compassView = (CompassView)findViewById(R.id.compass);
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        updateOrientation(new float[]{0,0,0});
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        Sensor magField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        sensorManager.registerListener(sensorEventListener,accelerometer,SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(sensorEventListener,magField,SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onStop() {
+        sensorManager.unregisterListener(sensorEventListener);
+        super.onStop();
+    }
+
+    private void updateOrientation(float[] values){
+        if(compassView != null){
+            compassView.setBearing(values[0]);
+            compassView.setPitch(values[1]);
+            compassView.setRoll(-values[2]);
+            compassView.invalidate();
+        }
+    }
+
+    private float[] calculateOrientation(){
+        float[] values = new float[3];
+        float[] R = new float[9];
+
+        SensorManager.getRotationMatrix(R, null, aValues, mValues);
+        SensorManager.getOrientation(R, values);
+
+        values[0] = (float)Math.toDegrees(values[0]);
+        values[1] = (float)Math.toDegrees(values[1]);
+        values[2] = (float)Math.toDegrees(values[2]);
+        return values;
     }
 }
--- src/view/CompassView.java
+++ src/view/CompassView.java
@@ -3,6 +3,7 @@ package view;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -19,6 +20,8 @@ public class CompassView extends View {
     private String southString;
     private String westString;
     private int textHeight;
+    private float pitch = 0;
+    private float roll = 0;
 
     public CompassView(Context context) {
         super(context);
@@ -39,7 +42,7 @@ public class CompassView extends View {
         setFocusable(true);
         Resources resources = getResources();
         circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        circlePaint.setColor(resources.getColor(R.color.background));
+        circlePaint.setColor(Color.BLACK);
         circlePaint.setStrokeWidth(1);
         circlePaint.setStyle(Paint.Style.FILL_AND_STROKE);
 
@@ -49,11 +52,12 @@ public class CompassView extends View {
         westString = resources.getString(R.string.cardinal_west);
 
         textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        textPaint.setColor(resources.getColor(R.color.text));
+        textPaint.setColor(Color.WHITE);
+        textPaint.setTextSize(18);
         textHeight = (int)textPaint.measureText("yY");
 
         markerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        markerPaint.setColor(resources.getColor(R.color.marker));
+        markerPaint.setColor(Color.LTGRAY);
     }
 
     @Override
@@ -86,11 +90,118 @@ public class CompassView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         int px = getMeasuredWidth()/2;
-        int py =  getMeasuredHeight()/2;
+        int py = getMeasuredHeight()/2;
 
         int radius = Math.min(px, py);
         canvas.drawCircle(px, py, radius, circlePaint);
         canvas.save();
         canvas.rotate(-bearing, px, py);
+        int textWidth = (int)textPaint.measureText(westString);
+        int cardinalX = -2 + px-textWidth/2;
+        int cardinalY = py-radius+textHeight;
+        for(int i = 0; i < 24; i++){
+            canvas.drawLine(px, py - radius, px, py - radius + 10, markerPaint);
+            canvas.save();
+            canvas.translate(0, textHeight);
+            textPaint.setTextSize(30);
+            if(i % 6 == 0){
+                textPaint.setColor(Color.WHITE);
+                String dirString = "";
+                int arrowY = 2*textHeight;
+                switch(i){
+                    case 0:{
+                        textPaint.setColor(Color.RED);
+                        textPaint.setFakeBoldText(true);
+                        dirString = northString;
+                        canvas.drawText(dirString, cardinalX-1, cardinalY, textPaint);
+                        markerPaint.setColor(Color.RED);
+                        canvas.drawLine(px, arrowY, px-5, 3*textHeight, markerPaint);
+                        canvas.drawLine(px, arrowY, px+5, 3*textHeight, markerPaint);
+                        canvas.drawLine(px, arrowY, px, 5*textHeight,markerPaint);
+                        markerPaint.setColor(Color.LTGRAY);
+                        break;
+                    }
+                    case 6 : {
+                        dirString = eastString;
+                        canvas.drawLine(px, arrowY, px, 5*textHeight,markerPaint);
+                        canvas.drawText(dirString, cardinalX, cardinalY, textPaint);
+                        break;
+                    }
+                    case 12:{
+                        dirString = southString;
+                        canvas.drawLine(px, arrowY, px, 5*textHeight,markerPaint);
+                        canvas.drawText(dirString, cardinalX, cardinalY, textPaint);
+                        break;
+                    }
+                    case 18:{
+                        dirString = westString;
+                        canvas.drawLine(px, arrowY, px, 5*textHeight,markerPaint);
+                        canvas.drawText(dirString, cardinalX-3, cardinalY, textPaint);
+                        break;
+                    }
+                }
+                textPaint.setFakeBoldText(false);
+            }
+            if(i % 3 == 0 && i != 0 && i != 6 && i != 12 && i != 18){
+                textPaint.setColor(Color.LTGRAY);
+                String angle = String.valueOf(i*15);
+                float angleTextWidth = textPaint.measureText(angle);
+                int angleTextX = (int)(px-angleTextWidth/2);
+                int angleTextY = py-radius+textHeight;
+                canvas.drawText(angle, angleTextX, angleTextY, textPaint);
+            }
+            else if(i != 0 && i != 6 && i != 12 && i != 18){
+                textPaint.setColor(Color.GRAY);
+                textPaint.setTextSize(18);
+                String angle = String.valueOf(i*15);
+                float angleTextWidth = textPaint.measureText(angle);
+                int angleTextX = (int)(px-angleTextWidth/2);
+                int angleTextY = py-radius+textHeight;
+                canvas.drawText(angle, angleTextX, angleTextY, textPaint);
+            }
+            canvas.restore();
+            canvas.rotate(15, px, py);
+        }
+        canvas.restore();
+
+//        int mMeasureWidth = 100;
+//        int mMeasureHeight = 100;
+//        RectF rollOval = new RectF((mMeasureWidth/3)-mMeasureWidth/7,
+//                                    (mMeasureHeight/2)-mMeasureWidth/7,
+//                                    (mMeasureWidth/3)-mMeasureWidth/7,
+//                                    (mMeasureHeight/2)-mMeasureWidth/7);
+//        markerPaint.setStyle(Paint.Style.STROKE);
+//        canvas.drawOval(rollOval, markerPaint);
+//        markerPaint.setStyle(Paint.Style.FILL);
+//        canvas.save();
+//        canvas.rotate(roll, mMeasureWidth/3, mMeasureHeight/2);
+//        canvas.drawArc(rollOval,0,180, false, markerPaint);
+//        canvas.restore();
+
+//        RectF pitchOval = new RectF((mMeasureWidth/3)-mMeasureWidth/7,
+//                                    (2*mMeasureHeight/2)-mMeasureWidth/7,
+//                                    (mMeasureWidth/3)-mMeasureWidth/7,
+//                                    (2*mMeasureHeight/2)-mMeasureWidth/7);
+//        markerPaint.setStyle(Paint.Style.STROKE);
+//        canvas.drawOval(pitchOval, markerPaint);
+//        markerPaint.setStyle(Paint.Style.FILL);
+//        canvas.drawArc(pitchOval, 0-pitch/2, 180+(pitch), false, markerPaint);
+//        markerPaint.setStyle(Paint.Style.STROKE);
+    }
+
+    public float getPitch() {
+        return pitch;
+    }
+
+    public void setPitch(float pitch) {
+        this.pitch = pitch;
+    }
+
+    public float getRoll() {
+        return roll;
+    }
+
+    public void setRoll(float roll) {
+        this.roll = roll;
     }
 }
